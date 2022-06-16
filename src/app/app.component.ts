404afd72306b51b8515be6f95039be6610feb9bd
bhaktichauhan3 <gaurichauhan333@gmail.com>
2022-06-16 20:58:02 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { IQuestion } from './questions';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'obs-project';
+  title : string = 'OpenBlue Sherlock';
+  questions: IQuestion[] = [
+    {
+      "questionId": 1,
+      "questionName": "Why is my energy bill so high?"
+    },
+    {
+      "questionId": 2,
+      "questionName": "Which configuration had the most impact on the energy bill this month?"
+    },
+    {
+      "questionId": 3,
+      "questionName": "Which faulty equipment had the most impact on the energy bill this month?"
+    },
+    ]
+  constructor(public fb: FormBuilder) { }
+  questionsForm = this.fb.group({
+    name: ['']
+  })
+  onSubmit() {
+
+    alert(JSON.stringify(this.questionsForm.value))
+  }
+
+
 }
